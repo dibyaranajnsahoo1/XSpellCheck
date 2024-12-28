a4dbff8aa18a4a6d755cd56d0f2420e20620a4f2
Dibya Ranjan sahoo <ranjandibya34512@gmail.com>
2024-12-28 11:50:43 +0530

    add

---
+++ x-spellcheck/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Xspellcheck from './Components/Xspellcheck';
+
+function App() {
+  return (
+    <div className="App">
+  <Xspellcheck/>
+    </div>
+  );
+}
+
+export default App;
